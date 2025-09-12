e6865a5fb6f6f60afc5146de7de33310c28ccef0
Logan R Whitt <logan.whitt@ku.edu>
2025-09-12 14:46:57 -0500

    Add files via upload

---
+++ password_program.js
@@ -0,0 +1,8 @@
+function testProgram(){
+
+    var usr_input = document.getElementById("password1").value;
+    document.getElementById("password1") = "123123";
+    document.write(usr_input);
+
+}
+testProgram()
