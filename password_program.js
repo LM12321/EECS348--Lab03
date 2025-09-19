d1ba99fbded0e41b4c2e889c1a82e511ab01ec8f
Logan R Whitt <logan.whitt@ku.edu>
2025-09-18 23:03:16 -0500

    Delete password_program.js

--- password_program.js
+++
@@ -1,8 +0,0 @@
-function testProgram(){
-
-    var usr_input = document.getElementById("password1").value;
-    document.getElementById("password1") = "123123";
-    document.write(usr_input);
-
-}
-testProgram()
